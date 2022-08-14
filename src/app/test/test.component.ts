b444f79218e1528be2018bfa2a84ab7de9f1684d
nk400 <navkr007@gmail.com>
2022-08-14 20:24:47 +0530

    ADDED: Interpolation done

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -2,14 +2,34 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-test',
-  templateUrl: './test.component.html',
-  styleUrls: ['./test.component.css']
+  template: `
+            <h2>
+            Welcome {{name}}
+            </h2>
+            <h2>{{2+2}}</h2>
+            <h2>{{"Welcome " + name}}</h2>
+            <h2>{{name.length}}</h2>
+            <h2>{{name.toUpperCase()}}</h2>
+            <h2>{{greetUser()}}</h2>
+           <!--  <h2>{{a=2+2}}</h2> // won't work-->
+           <!-- <h2>{{window.location.href}}</h2> // won't work -->
+           <h2>{{siteUrl}}</h2>
+            `,
+  styleUrls: []
 })
 export class TestComponent implements OnInit {
+
+  public name = "codevolution";
+  public siteUrl = window.location.href;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+
+  greetUser(){
+    return "Hello with methods from component class" + this.name;
   }
 
 }
